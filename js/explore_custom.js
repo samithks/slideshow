bf092fedf6bc6cd88133611c6bd3c2ce574e01d3
samith <samithsarasan@gmail.com>
2013-05-28 21:21:03 +0530

    explore kerala

--- js/explore_custom.js
+++ js/explore_custom.js
@@ -14,4 +14,53 @@ $(document).ready(function()
 
 
   });
- });     
+ });  
+ 
+ 
+ /*modal */
+var $modal = $("#about_kerala_modal");
+
+$('#about_kerala').on('click', function(){
+  // create the backdrop and wait for next modal to be triggered
+  console.log("clicked");
+  
+  $('body').modalmanager('loading');
+  
+	setTimeout(function(){
+	     $modal.load('ajax_content/explore_kerala/about_kerala.html', '', function(){
+	      $modal.modal();
+	    });
+	  }, 1000);
+});   
+
+
+var $modal = $("#destinations_modal");
+
+$('#destinations').on('click', function(){
+  // create the backdrop and wait for next modal to be triggered
+  console.log("clicked");
+  
+  $('body').modalmanager('loading');
+  
+	setTimeout(function(){
+	     $modal.load('ajax_content/explore_kerala/destinations.html', '', function(){
+	      $modal.modal();
+	    });
+	  }, 1000);
+});   
+
+
+var $modal = $("#your_kerala_modal");
+
+$('#your_kerala').on('click', function(){
+  // create the backdrop and wait for next modal to be triggered
+  console.log("clicked");
+  
+  $('body').modalmanager('loading');
+  
+	setTimeout(function(){
+	     $modal.load('ajax_content/explore_kerala/your_kerala.html', '', function(){
+	      $modal.modal();
+	    });
+	  }, 1000);
+});   
