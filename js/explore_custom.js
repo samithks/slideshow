2537747da2325581c11fc90ac26378377db1103f
samith <samithsarasan@gmail.com>
2013-05-29 20:30:15 +0530

    sudden changes

--- js/explore_custom.js
+++ js/explore_custom.js
@@ -1,15 +1,15 @@
 $(document).ready(function()
 {
   $('#tour_planner-button').click(function() {
-    if($(this).css("margin-left") == "600px")
+    if($(this).css("margin-left") == "700px")
     {
-        $('.tour_planner').animate({"margin-left": '-=600'},900);
-        $('#tour_planner-button').animate({"margin-left": '-=600'},900);
+        $('.tour_planner').animate({"margin-left": '-=700'},900);
+        $('#tour_planner-button').animate({"margin-left": '-=700'},900);
     }
     else
     {
-        $('.tour_planner').animate({"margin-left": '+=600'},900);
-        $('#tour_planner-button').animate({"margin-left": '+=600'},900);
+        $('.tour_planner').animate({"margin-left": '+=700'},900);
+        $('#tour_planner-button').animate({"margin-left": '+=700'},900);
     }
 
 
